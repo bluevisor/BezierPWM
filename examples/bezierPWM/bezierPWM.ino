d9c4e01b0a94708ca3d3c0bbf54ca2f83d93b4e6
John Zheng <bluevisor@gmail.com>
2023-10-26 11:26:51 -0700

    fixed untracked files

--- examples/bezierPWM/bezierPWM.ino
+++ examples/bezierPWM/bezierPWM.ino
@@ -9,24 +9,26 @@ void setup() {
 
 void loop() {
     // Using predefined curve "ease," fade from 0 to 1 in 1 second
-    pwm.ease(0, 1, 1000);
+    // pwm.ease(0, 1, 1000);
     
-    // Using predefined curve "easeIn," fade from 1 to 0 in 1 second
-    pwm.easeIn(1, 0, 1000);
+    // // Using predefined curve "easeIn," fade from 1 to 0 in 1 second
+    // pwm.easeIn(1, 0, 1000);
     
-    delay(500);
+    // delay(500);
     
-    // Using predefined curve "easeOut," fade from 0 to 1 in 1 second
-    pwm.easeOut(0, 1, 1000);
+    // // Using predefined curve "easeOut," fade from 0 to 1 in 1 second
+    // pwm.easeOut(0, 1, 1000);
     
-    // Using predefined curve "easeInOut," fade from 1 to 0 in 2 seconds
-    pwm.easeInOut(1, 0, 2000);
-    
-    delay(500);
+    // // Using predefined curve "easeInOut," fade from 1 to 0 in 2 seconds
+    // pwm.easeInOut(1, 0, 2000);
+    pwm.breathIn(0, 1, 2300);
+    delay(200);
+    pwm.breathOut(1, 0, 2300);
+    delay(800);
     
     // Using custom control points, fade from 1 to 0 in 2 seconds
-    float customPoints[] = {0.2, 0.1, 0.6, 0.9};
-    pwm.bezierFade(customPoints, 1, 0, 2000);
+    // float customPoints[] = {0.2, 0.1, 0.6, 0.9};
+    // pwm.bezierFade(customPoints, 1, 0, 2000);
     
-    delay(500);
+    // delay(500);
 }
